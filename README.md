6cb8ff78fa7be4a8cf9357851d22ba0549f84a14
Fujie Zhang <878279297@qq.com>
2020-12-23 09:52:57 +0800

    Created project.
    Signed-off-by: Fujie Zhang <878279297@qq.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# taco-cloud
+Spring boot projects demo
+I will try out various spring modules:
+1. Core
+2. MVC
+3. Security
+4. JPA
+  - Hibernate
+  - JDBC
+5. Thymeleaf
+6. Logging
+
+I may also touch on WebFlux, batch processing and cloud computing.
---
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,22 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Fujie Zhang
+ *
+ */
+@SpringBootApplication
+public class TacoCloudApplication
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
